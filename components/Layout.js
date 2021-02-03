1399098fd4a57031d5ba49abdcf3ba88364dfbc6
Conor Walsh <conorwalsh0703@gmail.com>
2021-02-03 19:47:54 +0000

    installed tailwind

---
+++ components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Layout.module.css';
+
+const Layout = () => {
+  return <div className={styles.container}>
+      <main className={}></main>
+  </div>;
+};
+
+export default Layout;
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import Head from 'next/head';
 
-export default function Home() {
+const about = () => {
   return (
     <div className="">
       <Head>WebDev News</Head>
       <meta name="keywords" content="web development, programming" />
-      <h1 className="bg-blue-300">Welcome to Next</h1>
+      <h1>About</h1>
     </div>
   );
-}
+};
+
+export default about;
